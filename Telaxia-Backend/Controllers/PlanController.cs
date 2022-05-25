31d92554972b3df16a06783ae11825c0482df776
Andres Reynoso <u202010196@upc.edu.pe>
2022-05-24 20:15:47 -0500

    Added first backend page version, items that work are the following: User, Post, Plan, UserType and Profile

---
+++ Telaxia-Backend/Controllers/PlanController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telaxia_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new object[] {
+                   new{UserType= 31, id=11, name="Free", Price="$0" },
+                   new{UserType= 31, id=12, name="Pro", Price="$19" },
+                   new{UserType= 31, id=13, name="Gigachad", Price="$29" },
+                   new{UserType= 31, id=14, name="Picaso", Price="$49" },
+
+                   new{UserType = 32, id=21, name= "Free", Price="$0" },
+                   new{UserType = 32, id=22, name= "Collector", Price="$19" },
+            });
+        }
+    }
+}
---
+++ Telaxia-Backend/Controllers/UserTypeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telaxia_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTypeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new object[] {
+                   new{id = 31, type="Designer" },
+                   new{id = 32, type="Buyer" }
+            });
+        }
+    }
+}
