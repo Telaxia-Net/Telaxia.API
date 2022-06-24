3377a794f06709e1f91e65b207ae331a540037b1
ErikRomeroPolli <U202019857@upc.edu.pe>
2022-06-24 01:08:57 -0500

    fix: plan controller

--- texlaxia-backend/Telaxia/Controllers/PlansController.cs
+++ texlaxia-backend/Telaxia/Controllers/PlansController.cs
@@ -10,72 +10,71 @@ namespace texlaxia_backend.Telaxia.Controllers;
 [Route("/api/v1/[controller]")]
 public class PlansController:ControllerBase
 {
-    private readonly ICommentService _commentService;
+    private readonly IPlanService _planService;
     private readonly IMapper _mapper;
 
-
-    public PlansController(ICommentService commentService, IMapper mapper)
+    public PlansController(IPlanService planService, IMapper mapper)
     {
-        _commentService = commentService;
+        _planService = planService;
         _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<IEnumerable<CommentResource>> GetAllAsync()
+    public async Task<IEnumerable<PlanResource>> GetAllAsync()
     {
-        var comments = await _commentService.ListAsync();
-        var resources = _mapper.Map<IEnumerable<Comment>, IEnumerable<CommentResource>>(comments);
+        var plans = await _planService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Plan>, IEnumerable<PlanResource>>(plans);
 
         return resources;
     }
 
     
     [HttpPost]
-    public async Task<IActionResult> PostAsync([FromBody] SaveCommentResource resource)
+    public async Task<IActionResult> PostAsync([FromBody] SavePlanResource resource)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState.GetErrorMessages());
 
-        var comment = _mapper.Map<SaveCommentResource, Comment>(resource);
+        var plan = _mapper.Map<SavePlanResource, Plan>(resource);
 
-        var result = await _commentService.SaveAsync(comment);
+        var result = await _planService.SaveAsync(plan);
 
         if (!result.Success)
             return BadRequest(result.Message);
 
-        var commentResource = _mapper.Map<Comment, CommentResource>(result.Resource);
+        var planResource = _mapper.Map<Plan, PlanResource>(result.Resource);
 
-        return Ok(commentResource);
+        return Ok(planResource);
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveCommentResource resource)
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SavePlanResource resource)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState.GetErrorMessages());
 
-        var comment = _mapper.Map<SaveCommentResource, Comment>(resource);
+        var plan = _mapper.Map<SavePlanResource, Plan>(resource);
 
-        var result = await _commentService.UpdateAsync(id, comment);
+        var result = await _planService.UpdateAsync(id, plan);
 
         if (!result.Success)
             return BadRequest(result.Message);
 
-        var commentResource = _mapper.Map<Comment, CommentResource>(result.Resource);
+        var planResource = _mapper.Map<Plan, PlanResource>(result.Resource);
 
-        return Ok(commentResource);
+        return Ok(planResource);
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAsync(int id)
     {
-        var result = await _commentService.DeleteAsync(id);
+        var result = await _planService.DeleteAsync(id);
 
         if (!result.Success)
             return BadRequest(result.Message);
 
-        var commentResource = _mapper.Map<Comment, CommentResource>(result.Resource);
+        var planResource = _mapper.Map<Plan, PlanResource>(result.Resource);
 
-        return Ok(commentResource);
+        return Ok(planResource);
     }
 }
